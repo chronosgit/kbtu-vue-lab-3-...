a1ec5b7b83f29f3257f2bfc13e9a9428eba0e277
chronosgit <warfacemeister@mail.ru>
2024-10-06 22:01:14 +0500

    ✨ feat: added registration page

---
+++ pages/auth/registration.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+	import MyHeader from '~/components/organisms/MyHeader.vue';
+	import RegistrationConfirmationPanel from '~/components/organisms/RegistrationConfirmationPanel.vue';
+	import RegistrationPanel from '~/components/organisms/RegistrationPanel.vue';
+
+	useHead({ title: 'Registration' });
+
+	const { form, isConfirmationPanelActive, onFormValueChange, toConfirmPanel } =
+		useRegistrationForm();
+</script>
+
+<template>
+	<div
+		class="h-screen w-screen bg-gradient-to-b from-[#84cae9] via-[#bddded] to-[#faf2f3]"
+	>
+		<div class="mb-8">
+			<MyHeader />
+		</div>
+
+		<div class="px-2">
+			<RegistrationPanel
+				v-if="!isConfirmationPanelActive"
+				:form
+				@form-value-change="onFormValueChange"
+				@form-submit="toConfirmPanel"
+			/>
+
+			<RegistrationConfirmationPanel v-else />
+		</div>
+	</div>
+</template>
