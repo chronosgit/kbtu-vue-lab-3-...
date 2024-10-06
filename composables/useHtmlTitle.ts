d7fb507f079494777f39011b2d4696a3a109ec2f
chronosgit <warfacemeister@mail.ru>
2024-10-06 15:56:02 +0500

    ➕ misc: removed unnecessary type declarations

--- composables/useHtmlTitle.ts
+++ composables/useHtmlTitle.ts
@@ -1,13 +1,11 @@
 export default function useHtmlTitle() {
 	const route = useRoute();
 
-	const title = ref<string>('Sophisticated blog');
+	const title = ref('Sophisticated blog');
 
 	watch(
 		() => route.meta.title,
 		(newTitle) => {
-			console.log(newTitle);
-
 			if (!newTitle) return;
 
 			title.value = newTitle as string;
