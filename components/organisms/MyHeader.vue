aff88396eca85094527b45bbd3905ea0f146b5bf
chronosgit <warfacemeister@mail.ru>
2024-10-06 18:48:48 +0500

    ✨ feat: updated organism-level MyHeader component

--- components/organisms/MyHeader.vue
+++ components/organisms/MyHeader.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+	import Dropdown from '../molecules/Dropdown.vue';
 	import IconBurger from '../atoms/IconBurger.vue';
 	import IconUser from '../atoms/IconUser.vue';
 
@@ -6,28 +7,53 @@
 		(e: 'burgerClick'): void;
 	}>();
 
-	const onUserClick = () => console.log('user');
+	const dropdownRef = useTemplateRef('dropdown');
+	const {
+		isActive: isDropdown,
+		activate: openDropdown,
+		disactivate: closeDropdown,
+	} = useClickaway(dropdownRef);
+
+	const onUserIconClick = () => {
+		if (isDropdown.value) closeDropdown();
+		else openDropdown();
+	};
 </script>
 
 <template>
 	<header
-		class="flex items-center justify-between gap-4 p-2 bg-white bg-opacity-45"
+		class="flex items-center justify-between gap-4 bg-white bg-opacity-45 p-2"
 	>
 		<div class="cursor-pointer" @click.stop="emit('burgerClick')">
 			<IconBurger />
 		</div>
 
 		<div
-			class="p-2 rounded-xl text-center bg-gradient-to-r from-[#e5f67c] via-[#dfe84d] to-[#eaed39]"
+			class="rounded-xl bg-gradient-to-r from-[#e5f67c] via-[#dfe84d] to-[#eaed39] p-2 text-center"
 		>
 			<span
-				class="font-tnr text-lg bg-gradient-to-r from-[#f9bf9d] via-[#ff9480] to-[#ff794f] bg-clip-text text-transparent"
+				class="bg-gradient-to-r from-[#f9bf9d] via-[#ff9480] to-[#ff794f] bg-clip-text font-tnr text-lg text-transparent"
 				>New trips on Fall season! Full details on our Instagram accounts</span
 			>
 		</div>
 
-		<div class="cursor-pointer" @click.stop="onUserClick">
-			<IconUser />
+		<div class="relative cursor-pointer">
+			<div @click.stop="onUserIconClick">
+				<IconUser />
+			</div>
+
+			<Dropdown ref="dropdown" :is-open="isDropdown" :snap-to-right="true">
+				<div
+					class="mt-1 text-center font-tnr text-xl font-bold uppercase text-[#548eff]"
+				>
+					<div class="border-[1px] border-black bg-[#43e567] p-2">
+						<p class="text-outline">Login</p>
+					</div>
+					<div class="border-[1px] border-black bg-[#f8f14d] p-2">
+						<p class="text-outline">Register</p>
+					</div>
+				</div>
+			</Dropdown>
 		</div>
 	</header>
 </template>
