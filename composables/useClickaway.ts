0aed60083a260f6fc043f23cda65360486bcd8bd
chronosgit <warfacemeister@mail.ru>
2024-10-06 16:49:59 +0500

    ✨ feat: added useClickaway composable

---
+++ composables/useClickaway.ts
@@ -0,0 +1,30 @@
+export default function (
+	templateRef: Ref<HTMLElement | ComponentPublicInstance | null>
+) {
+	const isActive = ref(false);
+
+	const activate = () => (isActive.value = true);
+
+	const onDocumentClick = (e: MouseEvent) => {
+		const clickedTarget = e.target as Element;
+
+		if (!clickedTarget || !templateRef?.value) return;
+
+		let checkedEl: HTMLElement | null = null;
+
+		if (templateRef.value instanceof HTMLElement) {
+			checkedEl = templateRef.value;
+		} else {
+			checkedEl = templateRef.value.$el;
+		}
+
+		if (checkedEl && !checkedEl.contains(clickedTarget)) {
+			isActive.value = false;
+		}
+	};
+
+	onMounted(() => document.addEventListener('click', onDocumentClick));
+	onUnmounted(() => document.removeEventListener('click', onDocumentClick));
+
+	return { activate, isActive };
+}
