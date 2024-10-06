4fa3ade81c2684d7df39c3796e9d3cf7ac74161d
chronosgit <warfacemeister@mail.ru>
2024-10-06 12:33:00 +0500

    ✨ feat: added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+	const route = useRoute();
+
+	const title = route.meta.title || 'Sophisticated blog';
+</script>
+
+<template>
+	<div>
+		<Html>
+			<Head>
+				<Title>{{ title }}</Title>
+			</Head>
+
+			<Body>
+				<slot />
+			</Body>
+		</Html>
+	</div>
+</template>
