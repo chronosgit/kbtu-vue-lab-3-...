35fd978b17432cc9577130f1b397986cd0177f40
chronosgit <warfacemeister@mail.ru>
2024-10-06 14:28:46 +0500

    ✨ feat: added atom-level Icon components
    Added IconBurger and IconUser

---
+++ components/atoms/IconBurger.vue
@@ -0,0 +1,12 @@
+<template>
+	<div
+		class="flex items-center justify-center p-2 bg-black rounded-full max-w-max"
+	>
+		<NuxtIcon
+			name="i-material-symbols:menu-rounded"
+			size="1.5rem"
+			mode="svg"
+			class="text-white fill-current"
+		/>
+	</div>
+</template>
---
+++ components/atoms/IconUser.vue
@@ -0,0 +1,7 @@
+<template>
+	<div
+		class="flex items-center justify-center p-2 bg-[#7eefff] rounded-full max-w-ma"
+	>
+		<NuxtIcon name="i-material-symbols:person" size="1.5rem" />
+	</div>
+</template>
