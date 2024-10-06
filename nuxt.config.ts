aa7b59d4e1d4831ff43efd13f7a9f406e7fc3fa0
chronosgit <warfacemeister@mail.ru>
2024-10-06 11:31:52 +0500

    🔧 config: updated nuxt config
    Added path alias for ~components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
 	compatibilityDate: '2024-04-03',
 	devtools: { enabled: true },
 
+	components: false,
+	alias: {
+		components: './components',
+	},
+
 	modules: ['@nuxtjs/tailwindcss', '@nuxt/icon'],
 
 	icon: {
