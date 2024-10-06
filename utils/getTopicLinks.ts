e83825f3cb6792a8f907c4a71257e220ef826955
chronosgit <warfacemeister@mail.ru>
2024-10-06 16:49:44 +0500

    🎨 style: refactored sidebar's topicLinks into an util

---
+++ utils/getTopicLinks.ts
@@ -0,0 +1,26 @@
+export default function () {
+	const topicLinks = [
+		{
+			text: 'Adventure blog',
+			url: '/topics/adventure',
+			twGradient: 'bg-gradient-to-r from-[#4cd97c] to-[#3dd55f]',
+		},
+		{
+			text: 'Nature blog',
+			url: '/topics/nature',
+			twGradient: 'bg-gradient-to-r from-[#5cde69] to-[#22cd1e]',
+		},
+		{
+			text: 'Fashion blog',
+			url: '/topics/fashion',
+			twGradient: 'bg-gradient-to-r from-[#79de56] to-[#49e521]',
+		},
+		{
+			text: 'Modern blog',
+			url: '/topics/modern',
+			twGradient: 'bg-gradient-to-r from-[#68e674] to-[#7dcc85]',
+		},
+	];
+
+	return topicLinks;
+}
