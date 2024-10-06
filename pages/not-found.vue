65ae3a4d4a6dff200686b85fa4a30795de1a2e70
chronosgit <warfacemeister@mail.ru>
2024-10-06 12:31:07 +0500

    ✨ feat: added not-found page

---
+++ pages/not-found.vue
@@ -0,0 +1,15 @@
+<template>
+	<div
+		class="px-2 w-screen h-screen bg-[url(~/assets/images/bg-trees.png)] bg-cover bg-center flex justify-center items-center"
+	>
+		<div class="p-8 text-center bg-white rounded-lg shadow-lg font-poppins">
+			<h1 class="text-3xl text-error-solid">Page not found</h1>
+
+			<p class="my-4 text-lg">Sorry, that page doesn't exist.</p>
+
+			<NuxtLink to="/" class="mt-6 text-blue-500 cursor-pointer">
+				Go to root
+			</NuxtLink>
+		</div>
+	</div>
+</template>
