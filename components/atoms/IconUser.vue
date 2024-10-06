00ad6a8dd0249885ecd48a15188fe942d11380bf
chronosgit <warfacemeister@mail.ru>
2024-10-06 15:49:14 +0500

    🩹 fix: fixed util class in IconUser

--- components/atoms/IconUser.vue
+++ components/atoms/IconUser.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="flex items-center justify-center p-2 bg-[#7eefff] rounded-full max-w-ma"
+		class="flex items-center justify-center p-2 bg-[#7eefff] rounded-full max-w-max"
 	>
 		<NuxtIcon name="i-material-symbols:person" size="1.5rem" />
 	</div>
