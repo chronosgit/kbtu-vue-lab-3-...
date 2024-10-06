d446dcd9bcbe1c13d2431cc1cbfba96b78f9817f
chronosgit <warfacemeister@mail.ru>
2024-10-06 13:42:53 +0500

    🚧 wip: working on /topic/:topic page
    Added basic logic (router param error handling AND dynamic title

---
+++ pages/topics/[topic].vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+	const {
+		params: { topic },
+	} = useRoute() as { params: { topic?: string } };
+
+	if (!topic) {
+		throw createError({
+			statusCode: 400,
+			statusMessage: 'Invalid topic: Topic must be specified.',
+			fatal: true,
+		});
+	}
+
+	useHead({ title: `${capitalize(topic)} blog` });
+</script>
+
+<template>
+	<div>
+		<h1>{{ topic }}</h1>
+	</div>
+</template>
