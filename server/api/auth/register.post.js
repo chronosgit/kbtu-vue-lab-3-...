35d4f2293fe993927f75aa84cfe037bf6db1db47
chronosgit <warfacemeister@mail.ru>
2024-10-07 22:36:39 +0500

    🚧 wip: working on the /api/auth/register POST endpoint

---
+++ server/api/auth/register.post.js
@@ -0,0 +1,19 @@
+import User from '~/server/models/User';
+
+export default defineEventHandler(async (e) => {
+	try {
+		const { email, username, password } = await readBody(e);
+
+		console.log(123);
+
+		// const res = new User({ email, username, password });
+
+		// await res.save();
+
+		return getSuccessResponse(201, 'Confirmation letter is sent', {});
+	} catch (err) {
+		console.error(err);
+
+		throw createError(getErrorOptions(err));
+	}
+});
