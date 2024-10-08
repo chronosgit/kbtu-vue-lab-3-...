0183c2def1955e2b4599d4d1fb3c1d080e1ed1c4
chronosgit <warfacemeister@mail.ru>
2024-10-08 22:22:25 +0500

    ✨ feat: added dummy email confirmation logic

--- server/api/auth/confirm-email.get.js
+++ server/api/auth/confirm-email.get.js
@@ -1,6 +1,27 @@
+import jwt from 'jsonwebtoken';
+import User from '~/server/models/User';
+
+// Dummy logic
 export default defineEventHandler(async (e) => {
 	try {
-		return getSuccessResponse(200, 'Letter sent');
+		const accessToken = getCookie(e, 'access_token');
+
+		if (!accessToken) {
+			throw createError({ statusCode: 401 });
+		}
+
+		const { ACCESS_TOKEN_SECRET } = useRuntimeConfig();
+
+		const { email } = jwt.verify(accessToken, ACCESS_TOKEN_SECRET);
+
+		const emailConfirmationToken = await User.findOne(
+			{ email },
+			{ emailConfirmationToken: 1, _id: 0 }
+		);
+
+		return getSuccessResponse(200, 'Letter sent', {
+			confirmationToken: emailConfirmationToken,
+		});
 	} catch (err) {
 		console.error(err);
 
