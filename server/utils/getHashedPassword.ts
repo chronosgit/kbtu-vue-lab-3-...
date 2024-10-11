d1d1a2a6f5d5632d77879a380294cd83607e18c6
chronosgit <warfacemeister@mail.ru>
2024-10-11 10:32:21 +0500

    ✨ feat: updated the getHashedPassword server util

--- server/utils/getHashedPassword.ts
+++ server/utils/getHashedPassword.ts
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
 
-export default async function (password: string) {
+export default async function async(password: string) {
 	const salt = await bcrypt.genSalt(10);
 	const hashedPassword = await bcrypt.hash(password, salt);
 
