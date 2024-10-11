4d26c0028fd728750cf68da7b9cbba8438a41a5f
chronosgit <warfacemeister@mail.ru>
2024-10-11 21:27:20 +0500

    ✨ feat: updated the /api/auth/login POST endpoint

---
+++ server/api/auth/login.post.ts
@@ -0,0 +1,56 @@
+import { ObjectId } from 'mongoose';
+import User from '~/server/models/User';
+import verifyPassword from '~/server/utils/verifyPassword';
+
+export default defineEventHandler(async (e) => {
+	try {
+		const { username, password } = await readBody(e);
+
+		const user = await User.findOne({ username });
+		if (!user) {
+			throw createError({
+				statusCode: 404,
+				statusMessage: "Such user doesn't exist",
+			});
+		}
+
+		if (!verifyPassword(password, user.password)) {
+			throw createError({
+				statusCode: 403,
+				statusMessage: 'Invalid password',
+			});
+		}
+
+		const { accessToken, refreshToken } = getTokens(
+			user._id as ObjectId,
+			user.email,
+			user.username
+		);
+
+		setCookie(e, 'access_token', accessToken, {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === 'production',
+			sameSite: 'strict',
+			maxAge: 3600, // def: 1h
+			path: '/',
+		});
+
+		setCookie(e, 'refresh_token', refreshToken, {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === 'production',
+			sameSite: 'strict',
+			maxAge: 60 * 60 * 24 * 30, // 30 days
+			path: '/',
+		});
+
+		return getSuccessResponse(200, 'Authenticated', {
+			id: user._id,
+			username,
+			email: user.email,
+		});
+	} catch (err) {
+		console.error(err);
+
+		throw createError(getErrorOptions(err));
+	}
+});
