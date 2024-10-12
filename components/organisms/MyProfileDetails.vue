ec779e6f9e72c04f16ce86a1c50b136397a77449
chronosgit <warfacemeister@mail.ru>
2024-10-13 00:54:55 +0500

    🎨 style: refactored MyProfileDetails into an organism

---
+++ components/organisms/MyProfileDetails.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+	const props = defineProps<{ username: string }>();
+
+	// const ageModel = defineModel<number>('age');
+	// const locationModel = defineModel<string>('location');
+</script>
+
+<template>
+	<div class="flex items-center gap-6 font-tnr">
+		<img
+			src="~/assets/images/avatar-placeholder.png"
+			alt="avatar"
+			class="max-w-60 rounded-full"
+		/>
+
+		<div class="space-y-4">
+			<p class="text-outline text-5xl text-blue-500">
+				{{ props.username }}
+			</p>
+
+			<!-- <div class="flex items-center gap-4">
+				<label
+					for="/users/me:input-age"
+					class="text-outline text-2xl text-yellow-300"
+				>
+					Age:
+				</label>
+
+				<SquareTextInput
+					v-model="ageModel"
+					id="/users/me/:input-age"
+					name="age"
+					placeholder="?"
+					class="max-w-12 text-center text-xl"
+				/>
+			</div> -->
+
+			<!-- <SquareTextInput
+				v-model="locationModel"
+				placeholder="Your location"
+				class="max-w-56 text-center text-xl"
+			/> -->
+
+			<p class="text-xl font-bold text-[#00ffa3] drop-shadow-md">
+				Activity: for now
+			</p>
+		</div>
+	</div>
+</template>
