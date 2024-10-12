89554d3a99fc1e03ee153919d3d12f2a08ee8419
chronosgit <warfacemeister@mail.ru>
2024-10-12 18:45:49 +0500

    ✨ feat: updated useMyProfile composable

--- composables/useMyProfile.ts
+++ composables/useMyProfile.ts
@@ -1,5 +1,22 @@
 import type IUser from '~/interfaces/IUser';
+import type CustomizeableProfileInputs from '~/enums/CustomizeableProfileInputs';
 
-export default function (initialUserState: IUser | null) {
-	onMounted(() => console.log(initialUserState));
+// WARN: possible logic flaw
+// Initial user state is a global state
+// Will its alterations change the profile value too?
+export default function (initialUserState: IUser) {
+	const profile = ref({
+		username: initialUserState.username,
+		age: initialUserState.age?.toString() || '',
+		location: initialUserState.location?.toString() || '',
+		lastLoggedIn: initialUserState.lastLoggedIn,
+		rating: initialUserState.rating,
+	});
+
+	const onInputEnterKey = (
+		input: CustomizeableProfileInputs,
+		value: string
+	) => {};
+
+	return { profile, onInputEnterKey };
 }
