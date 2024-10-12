3539e4248b879c1566a2636ed01598bc1d850488
chronosgit <warfacemeister@mail.ru>
2024-10-12 13:33:47 +0500

    🚧 wip: working on /users/me page

--- pages/users/me.vue
+++ pages/users/me.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 	import useUserStore from '~/store/useUserStore';
 
+	definePageMeta({ middleware: 'auth' });
+	useHead({ title: 'My profile' });
+
 	const { user } = useUserStore();
 </script>
 
