087fb4a7b10744342bf62c9435d75ad8bcfb0d55
chronosgit <warfacemeister@mail.ru>
2024-10-12 13:34:03 +0500

    ✨ feat: updated the user store

--- store/useUserStore.ts
+++ store/useUserStore.ts
@@ -1,22 +1,33 @@
 import type IUser from '~/interfaces/IUser';
 
 export default function () {
-	const isAuthenticated = useState('isAuthenticated', () => false);
+	const isAuthenticated = useState<boolean | null>(
+		'isAuthenticated',
+		() => null
+	);
 	const user = useState<IUser | null>('user', () => ({
 		id: null,
 		username: null,
 		email: null,
 		lastLoggedIn: null,
+		likes: null,
+		isEmailConfirmed: null,
 	}));
 
-	const loginUser = (loggedUser: IUser) => (user.value = { ...loggedUser });
+	const loginUser = (loggedUser: IUser) => {
+		isAuthenticated.value = true;
+		user.value = { ...loggedUser };
+	};
 
 	const logoutUser = () => {
+		isAuthenticated.value = false;
 		user.value = {
 			id: null,
 			username: null,
 			email: null,
 			lastLoggedIn: null,
+			likes: null,
+			isEmailConfirmed: null,
 		};
 	};
 
