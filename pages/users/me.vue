0ee530f11d12541e792ad18b31443066f4268908
chronosgit <warfacemeister@mail.ru>
2024-10-12 18:47:07 +0500

    🚧 wip: working on /users/me page UI

--- pages/users/me.vue
+++ pages/users/me.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 	import RegularTextInput from '~/components/atoms/RegularTextInput.vue';
 	import MyHeader from '~/components/organisms/MyHeader.vue';
-	import useUserStore from '~/store/useUserStore';
+	import CustomizeableProfileInputs from '~/enums/CustomizeableProfileInputs';
+	import useUserStore from '~/store/useCurrentUserStore';
 
-	definePageMeta({ middleware: 'auth' });
+	definePageMeta({ middleware: '4-protect-route' });
 	useHead({ title: 'My profile' });
 
 	const { user } = useUserStore();
 
-	const none = useMyProfile(user.value);
+	const { profile, onInputEnterKey } = useMyProfile(user.value);
 </script>
 
 <template>
@@ -20,35 +21,48 @@
 		</div>
 
 		<main class="px-4">
-			<h1 class="text-right font-tnr text-5xl text-white">My Profile</h1>
+			<h1 class="mb-5 text-right font-tnr text-5xl text-white">My Profile</h1>
 
-			<div class="flex items-center justify-between">
+			<div class="flex items-start justify-between">
 				<div class="">
 					<!-- Avatar -->
 
 					<div class="space-y-2 font-tnr">
 						<p class="text-outline text-3xl tracking-widest text-pink-500">
-							{{ user?.username }}
+							{{ user.username }}
 						</p>
 
-						<p
-							v-if="user?.age != null"
-							class="text-outline text-lg text-yellow-300"
-						>
+						<p v-if="user.age" class="text-outline text-lg text-yellow-300">
 							Age: {{ user?.age }}
 						</p>
-						<!-- <RegularTextInput
+						<RegularTextInput
 							v-else
-							v-model:model-value="age"
+							v-model="profile.age"
+							:type="CustomizeableProfileInputs.AGE"
 							placeholder="Add your age"
-							@on-enter-key="onAgeInputEnterClick"
+							class="text-outline text-lg text-yellow-300 placeholder-yellow-300"
+							@enter-key="onInputEnterKey"
+						/>
+
+						<!-- <p
+							v-if="user.location"
+							class="text-outline text-lg text-yellow-300"
+						>
+							Location: {{ user?.location }}
+						</p>
+						<RegularTextInput
+							v-else
+							v-model="profile.location"
+							placeholder="Add your location"
+							class="text-outline text-lg text-yellow-300 placeholder-yellow-300"
+							@enter-key=""
 						/> -->
 
-						<p>Location: {{ user?.location }}</p>
+						<!-- <p class="text-outline text-lg font-bold text-green-300">
+							Activity: for now
+						</p> -->
 
-						<p>Activity: for now</p>
-
-						<p>Current rating: {{ user?.rating }}</p>
+						<!-- <p>Current rating: {{ user.rating }}</p> -->
 					</div>
 				</div>
 
