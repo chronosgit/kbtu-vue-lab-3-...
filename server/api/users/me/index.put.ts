3d161710345c5d6bb09a2203ca56b9eb8d91c0ed
chronosgit <warfacemeister@mail.ru>
2024-10-14 18:52:26 +0500

    ✨ feat: updated /api/users/me PUT endpoint

--- server/api/users/me/index.put.ts
+++ server/api/users/me/index.put.ts
@@ -1,8 +1,31 @@
+import User from '~/server/models/User';
+
 export default defineEventHandler(async (e) => {
 	try {
+		const accessToken = getCookie(e, 'access_token');
+
+		if (!accessToken) {
+			throw createError({ statusCode: 400, statusMessage: 'No access token' });
+		}
+
+		const { email } = verifyAccessToken(accessToken);
+
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			throw createError({
+				statusCode: 404,
+				statusMessage: "Such user doesn't exist",
+			});
+		}
+
 		const { age, location } = await readBody(e);
 
-		return getSuccessResponse(200, 'User updated', { age, location });
+		user.age = age;
+		user.location = location;
+		await user.save();
+
+		return getSuccessResponse(200, 'User updated');
 	} catch (err) {
 		console.error(err);
 
