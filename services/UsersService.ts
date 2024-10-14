196270a55a84dc2a7dbc151a3a574a5bbb21667b
chronosgit <warfacemeister@mail.ru>
2024-10-14 20:15:04 +0500

    ✨ feat: updated the UsersService

--- services/UsersService.ts
+++ services/UsersService.ts
@@ -2,6 +2,10 @@ class UsersService {
 	static async getMe(age: string, location: string) {
 		return $fetch('/api/users/me', { method: 'PUT', body: { age, location } });
 	}
+
+	static getUser(userId: string) {
+		return $fetch(`/api/users/${userId}`);
+	}
 }
 
 export default UsersService;
