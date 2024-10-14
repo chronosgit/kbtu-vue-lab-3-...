1d5c8b863da60358f8eaa619a61bf54f5562ce66
chronosgit <warfacemeister@mail.ru>
2024-10-14 15:41:37 +0500

    ✨ feat: updated useLoginForm composable

--- composables/useLoginForm.ts
+++ composables/useLoginForm.ts
@@ -1,3 +1,7 @@
+import useCurrentUserStore from '~/store/useCurrentUserStore';
+import AuthService from '~/services/AuthService';
+import type IMyUser from '~/interfaces/IMyUser';
+
 export default function () {
 	const form = ref({
 		username: '',
@@ -7,6 +11,8 @@ export default function () {
 	const isLoading = ref(false);
 	const error = ref('');
 
+	const { loginUser } = useCurrentUserStore();
+
 	const resetStates = () => {
 		form.value.username = '';
 		form.value.password = '';
@@ -14,5 +20,27 @@ export default function () {
 		error.value = '';
 	};
 
-	return { form, error, isLoading, resetStates };
+	const { execute: login } = useLazyAsyncData(
+		'login',
+		() =>
+			AuthService.login(form.value.username, form.value.password)
+				.then(async (res) => {
+					const { user } = res.data as { user: IMyUser };
+					loginUser(user);
+
+					await navigateTo('/users/me');
+				})
+				.catch((err) => (error.value = err.message))
+				.finally(() => resetStates()),
+		{ immediate: false }
+	);
+
+	const onFormSubmit = () => {
+		isLoading.value = true;
+		error.value = '';
+
+		login();
+	};
+
+	return { form, error, isLoading, resetStates, onFormSubmit };
 }
