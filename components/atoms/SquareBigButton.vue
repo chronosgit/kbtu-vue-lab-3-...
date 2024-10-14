da2e70140cfd96a50fa4e148ff26d2e9385b0710
chronosgit <warfacemeister@mail.ru>
2024-10-14 18:44:08 +0500

    ✨ feat: added SquareBigButton atom

---
+++ components/atoms/SquareBigButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+	const emit = defineEmits<{
+		(e: 'click'): void;
+	}>();
+</script>
+
+<template>
+	<button
+		class="text-outline bg-[#6bebfc] px-4 py-1 text-3xl font-bold text-white drop-shadow-md"
+		@click="emit('click')"
+	>
+		<slot></slot>
+	</button>
+</template>
