f6562f9c64236339750a908ce92733a1a5382970
chronosgit <warfacemeister@mail.ru>
2024-10-15 17:19:28 +0500

    🚧 wip: working on /api/users/:id/follow PUT endpoint

--- server/api/users/follow.put.ts
+++ server/api/users/follow.put.ts
@@ -1,3 +1,6 @@
+import { isValidObjectId, mongo } from 'mongoose';
+import User from '~/server/models/User';
+
 export default defineEventHandler(async (e) => {
 	try {
 		const decoded = e.context.decodedToken;
@@ -8,7 +11,17 @@ export default defineEventHandler(async (e) => {
 
 		const { userId } = await readBody(e);
 
-		return getSuccessResponse(200, 'User followed', { decoded, userId });
+		if (!isValidObjectId(userId)) {
+			throw createError({
+				statusCode: 400,
+				statusMessage: 'Invalid user ID',
+			});
+		}
+
+		const targetUser = await User.findOne({ _id: new mongo.ObjectId(userId) });
+		const me = await User.findOne({ email: decoded.email });
+
+		return getSuccessResponse(200, 'User followed', { me, targetUser });
 	} catch (err) {
 		console.error(err);
 
