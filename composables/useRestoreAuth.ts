661422d3089bce9878055b220b1aab9005178c7d
chronosgit <warfacemeister@mail.ru>
2024-10-15 19:20:48 +0500

    ✨ feat: updated useRestoreAuth composable

--- composables/useRestoreAuth.ts
+++ composables/useRestoreAuth.ts
@@ -5,18 +5,22 @@ import type IMyUser from '~/interfaces/IMyUser';
 export default function () {
 	const { loginUser } = useCurrentUserStore();
 
-	useLazyAsyncData('restoreAuth', async () => {
-		try {
-			const res = await AuthService.restoreAuthentication();
-			const { user } = res.data as { user: IMyUser };
+	useLazyAsyncData(
+		'restoreAuth',
+		async () => {
+			try {
+				const res = await AuthService.restoreAuthentication();
+				const { user } = res.data as { user: IMyUser };
 
-			loginUser(user);
+				loginUser(user);
 
-			return res;
-		} catch (err) {
-			console.error(err);
+				return res;
+			} catch (err) {
+				console.error(err);
 
-			return null;
-		}
-	});
+				return null;
+			}
+		},
+		{ server: false }
+	);
 }
