12e281b6f32e13c4eb3242f6018933c5733dc45e
chronosgit <warfacemeister@mail.ru>
2024-10-15 20:17:49 +0500

    ✨ feat: added /api/users/followings.get.ts

---
+++ server/api/users/me/followings.get.ts
@@ -0,0 +1,37 @@
+import IAccessToken from '~/interfaces/IAccessToken';
+import User from '~/server/models/User';
+
+export default defineEventHandler(async (e) => {
+	try {
+		const decoded = e.context.decodedToken as IAccessToken;
+
+		if (!decoded) {
+			throw createError({
+				statusCode: 401,
+				statusMessage: 'No access token',
+			});
+		}
+
+		const my = await User.findOne({ email: decoded.email });
+
+		if (!my) {
+			throw createError({
+				statusCode: 404,
+				statusMessage: "Such user doesn't exist",
+			});
+		}
+
+		const users = await User.find(
+			{ _id: { $in: my.followings } },
+			{ password: false }
+		);
+
+		return getSuccessResponse(200, 'My followings received', {
+			followings: users,
+		});
+	} catch (err) {
+		console.error(err);
+
+		throw createError(getErrorOptions(err));
+	}
+});
