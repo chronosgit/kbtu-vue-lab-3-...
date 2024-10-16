f9246bc06f06b9aa7d17a9a0c77e7e2c1385f2af
chronosgit <warfacemeister@mail.ru>
2024-10-16 15:49:30 +0500

    🎨 style: re-ordered tw classes in error.vue

--- error.vue
+++ error.vue
@@ -7,9 +7,9 @@
 <template>
 	<NuxtLayout>
 		<div
-			class="flex items-center justify-center w-screen h-screen px-2 bg-center bg-cover bg-trees"
+			class="flex h-screen w-screen items-center justify-center bg-trees bg-cover bg-center px-2"
 		>
-			<div class="p-8 text-center bg-white rounded-lg shadow-lg font-poppins">
+			<div class="rounded-lg bg-white p-8 text-center font-poppins shadow-lg">
 				<template v-if="error?.statusCode === 404">
 					<h1 class="text-3xl text-error-solid">Page not found</h1>
 
@@ -19,12 +19,12 @@
 				<template v-else>
 					<h1 class="text-3xl">Something just broke...</h1>
 
-					<p class="my-4 text-xl text-ember-600 text-error-solid">
+					<p class="text-ember-600 my-4 text-xl text-error-solid">
 						<strong>{{ error?.message }}</strong>
 					</p>
 				</template>
 
-				<p class="mt-6 text-blue-500 cursor-pointer" @click="handleError">
+				<p class="mt-6 cursor-pointer text-blue-500" @click="handleError">
 					Go to root
 				</p>
 			</div>
