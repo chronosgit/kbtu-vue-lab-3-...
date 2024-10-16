cc4d968661edff2b135e4ae4602f9a9d1b795f51
chronosgit <warfacemeister@mail.ru>
2024-10-16 18:47:33 +0500

    ✨ feat: added PostComposeForm molecule

---
+++ components/molecules/PostComposeForm.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+	import SquareTextInput from '../atoms/SquareTextInput.vue';
+
+	const { postDescr, onFormSubmit } = usePostComposeForm();
+</script>
+
+<template>
+	<form class="flex max-w-sm flex-col gap-4" @submit.prevent="onFormSubmit()">
+		<div class="flex flex-wrap items-center justify-between gap-2">
+			<label
+				for="description"
+				class="text-outline text-lg font-bold text-blue-300"
+				>Description:
+			</label>
+			<SquareTextInput
+				v-model="postDescr"
+				placeholder="What a wonderful day!"
+			/>
+		</div>
+
+		<button
+			class="text-outline self-end rounded-md bg-yellow-400 px-4 py-2 font-bold text-white"
+		>
+			Compose
+		</button>
+	</form>
+</template>
