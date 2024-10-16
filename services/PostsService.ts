42a4085c2bea7b426f67abef61c6c23a4992dd33
chronosgit <warfacemeister@mail.ru>
2024-10-16 18:46:40 +0500

    ✨ feat: updated the PostsService

--- services/PostsService.ts
+++ services/PostsService.ts
@@ -2,6 +2,10 @@ class PostsService {
 	static getPosts(page: number, pageSize: number, filter: string) {
 		return $fetch('/api/posts', { params: { page, pageSize, filter } });
 	}
+
+	static createPost(description: string) {
+		return $fetch('/api/posts', { method: 'POST', body: { description } });
+	}
 }
 
 export default PostsService;
