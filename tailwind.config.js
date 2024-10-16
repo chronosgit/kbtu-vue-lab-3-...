b33763656435e35e85a74012c6ea1add0bb13246
chronosgit <warfacemeister@mail.ru>
2024-10-16 22:31:56 +0500

    🔧 config: updated the tailwind  config

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,5 +24,14 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		function ({ addUtilities }) {
+			addUtilities({
+				'.clip-path-chevron': {
+					clipPath:
+						'polygon(50% 0%, 100% 20%, 100% 100%, 50% 80%, 0% 100%, 0% 20%)',
+				},
+			});
+		},
+	],
 };
