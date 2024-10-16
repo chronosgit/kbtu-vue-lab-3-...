3ced1719c2a793ad528f1ce9ee372b710de2f85c
chronosgit <warfacemeister@mail.ru>
2024-10-16 19:41:28 +0500

    ✨ feat: added PostCard molecule

---
+++ components/molecules/PostCard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+	import type IPost from '~/interfaces/IPost';
+	import RatingStars from './RatingStars.vue';
+
+	const props = defineProps<{
+		post: IPost;
+	}>();
+
+	const ts = computed(() =>
+		convertDateTimeToPostHistoryString(props.post.createdAt)
+	);
+</script>
+
+<template>
+	<div class="flex items-center justify-between bg-[#5bb9cd] p-2">
+		<div class="space-y-4 rounded-md bg-[#73c3d3] p-2">
+			<p class="text-lg font-medium text-white">
+				{{ props.post.authorUsername }}
+			</p>
+
+			<p class="text-lg font-medium text-white">{{ ts }}</p>
+		</div>
+
+		<div class="space-y-5">
+			<p class="text-center text-lg font-medium text-white">Rating</p>
+
+			<RatingStars :rating="props.post.rating" />
+		</div>
+
+		<img
+			class="max-h-24 max-w-24"
+			src="~/assets/images/avatar-placeholder.png"
+			alt="avatar"
+		/>
+	</div>
+</template>
