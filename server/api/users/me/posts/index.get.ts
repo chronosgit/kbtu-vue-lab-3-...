9712174fea4b2ae5587d63a32d91bcaf08716ffa
chronosgit <warfacemeister@mail.ru>
2024-10-18 20:53:11 +0500

    ✨ feat: updated the /users/me/posts GET endpoint

--- server/api/users/me/posts/index.get.ts
+++ server/api/users/me/posts/index.get.ts
@@ -14,10 +14,59 @@ export default defineEventHandler(async (e) => {
 			});
 		}
 
-		const myPosts = await Post.find({ authorId: decoded.id }).lean();
+		const { page: qPage, pageSize: qPageSize } = getQuery(e);
 
-		const postsToReturn = myPosts.map((mp) => ({ ...mp, id: mp._id }));
-		return getSuccessResponse(200, 'Posts received', { posts: postsToReturn });
+		const page = Number(qPage);
+		const pageSize = Number(qPageSize);
+
+		if (
+			Number.isNaN(page) ||
+			Number.isNaN(pageSize) ||
+			page < 1 ||
+			pageSize < 1
+		) {
+			throw createError({
+				statusCode: 400,
+				statusMessage: 'Invalid query parameters',
+			});
+		}
+
+		const totalPosts = await Post.countDocuments({ authorId: decoded.id });
+
+		if (totalPosts === 0) {
+			const response = {
+				posts: [],
+				meta: {
+					totalPosts: 0,
+					totalPages: 0,
+					currentPage: page,
+					postsPerPage: pageSize,
+					hasPreviousPage: false,
+					hasNextPage: false,
+				},
+			};
+
+			return getSuccessResponse(200, 'Posts received', response);
+		}
+
+		const myPosts = await Post.find({ authorId: decoded.id })
+			.skip((page - 1) * pageSize)
+			.limit(pageSize)
+			.lean();
+
+		const response = {
+			posts: myPosts,
+			meta: {
+				totalPosts,
+				totalPages: Math.ceil(totalPosts / pageSize),
+				currentPage: page,
+				postsPerPage: pageSize,
+				hasPreviousPage: page > 1,
+				hasNextPage: page < Math.ceil(totalPosts / pageSize),
+			},
+		};
+
+		return getSuccessResponse(200, 'Posts received', response);
 	} catch (err) {
 		console.error(err);
 
