1adfe46168464fb1dfe5bcb0612c728b7a3abf44
chronosgit <warfacemeister@mail.ru>
2024-10-18 20:09:27 +0500

    ✨ feat: updated the PostCompsoeForm molecule

--- components/molecules/PostComposeForm.vue
+++ components/molecules/PostComposeForm.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
 	import SquareTextInput from '../atoms/SquareTextInput.vue';
 
-	const { postDescr, onFormSubmit } = usePostComposeForm();
+	const { postDescr, topic, onFormSubmit } = usePostComposeForm();
 </script>
 
 <template>
 	<form class="flex max-w-sm flex-col gap-4" @submit.prevent="onFormSubmit()">
+		<select
+			v-model="topic"
+			required
+			class="bg-[#45d2ff] p-2 text-lg font-bold text-white"
+		>
+			<option value="" disabled>Select a topic</option>
+			<option value="ADVENTURE">Adventure</option>
+			<option value="NATURE">Nature</option>
+			<option value="FASHION">Fashion</option>
+			<option value="MODERN">Modern</option>
+		</select>
+
 		<div class="flex flex-wrap items-center justify-between gap-2">
 			<label
 				for="description"
